95b3e718769e702606a9c39a535832249b241258
qweliant <dmtorcode@gmail.com>
2023-06-09 03:55:51 -0400

    almost somewhere

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -1,9 +1,9 @@
 <script>
-  import Tiptap from "./lib/TipTap.svelte";
+  import Editor from "./lib/Editor.svelte";
 </script>
 
 <main>
   <h1>Note</h1>
   <h3>Take a note, save a note, merge notes</h3>
-  <Tiptap />
+  <Editor />
 </main>
---
+++ frontend/src/lib/Editor.svelte
@@ -0,0 +1,47 @@
+<script>
+  import { onMount, onDestroy } from "svelte";
+  import EditorJS from "@editorjs/editorjs";
+
+  import SimpleImage from "@editorjs/simple-image";
+  import Header from "@editorjs/header";
+  import List from "@editorjs/list";
+
+  let editor;
+
+  onMount(() => {
+    // Initialize the EditorJS instance
+    editor = new EditorJS({
+      holder: "editor",
+
+      // Configure the desired tools
+      tools: {
+        header: {
+          class: Header,
+          inlineToolbar: true,
+        },
+        list: {
+          class: List,
+          inlineToolbar: true,
+        },
+        image: SimpleImage,
+      },
+      autofocus: true,
+    });
+  });
+
+  // Destroy the editor instance when the component is unmounted
+  onDestroy(() => {
+    editor?.destroy();
+  });
+</script>
+
+<div id="editor" />
+
+<style>
+  /* Add any desired CSS styles for the editor container */
+  #editor {
+    border: 1px solid #ccc;
+    padding: 10px;
+    text-align: left;
+  }
+</style>
