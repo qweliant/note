a0c5b6ca18d8d2ee3e694a2630ef1615f69d3f67
qweliant <dmtorcode@gmail.com>
2023-07-10 00:27:32 -0400

    solve protocol buffer type issue

--- backend/app/io.go
+++ backend/app/io.go
@@ -1,73 +1,34 @@
 package app
 
 import (
-	"encoding/binary"
-	"io"
-	"os"
+	"io/ioutil"
+	"log"
 
 	"google.golang.org/protobuf/proto"
 )
 
 func WriteProtoToFile(filepath string, note *EditorNote) error {
-	outFile, err := os.OpenFile(filepath, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0666)
+	n := toProto(note)
+	// ...
+
+	// Write the new address book back to disk.
+	out, err := proto.Marshal(n)
 	if err != nil {
-		return err
+		log.Fatalln("Failed to encode address book:", err)
 	}
-
-	convertedNote := toProto(note)
-	encodedData, err := proto.Marshal(convertedNote)
-	if err != nil {
-		return err
+	if err := ioutil.WriteFile("raw", out, 0644); err != nil {
+		log.Fatalln("Failed to write address book:", err)
 	}
-
-	// writing the length of the encoded item before writing the data
-	buf := make([]byte, 4)
-	binary.LittleEndian.PutUint32(buf, uint32(len(encodedData)))
-	if _, err := outFile.Write(buf); err != nil {
-		return err
-	}
-
-	// writing the actual transaction item to the file
-	if _, err := outFile.Write(encodedData); err != nil {
-		return err
-	}
-
-	return nil
 }
 
 func ReadProtoFromFile(filepath string) ([][]byte, error) {
-	file, err := os.Open(filepath)
+	// Read the existing address book.
+	in, err := ioutil.ReadFile("raw")
 	if err != nil {
-		return nil, err
+		log.Fatalln("Error reading file:", err)
 	}
-
-	var offset int64
-	content := make([][]byte, 0)
-
-	for {
-		// reading the length of the encoded item before reading each item
-		buf := make([]byte, 4)
-		if _, err := file.ReadAt(buf, offset); err != nil {
-			if err == io.EOF {
-				break
-			}
-			return nil, err
-		}
-		itemSize := binary.LittleEndian.Uint32(buf)
-		offset += 4
-
-		// reading the actual encoded item
-		item := make([]byte, itemSize)
-		if _, err := file.ReadAt(item, offset); err != nil {
-			if err == io.EOF {
-				break
-			}
-			return nil, err
-		}
-
-		content = append(content, item)
-		offset += int64(itemSize)
+	n := &Note{}
+	if err := proto.Unmarshal(in, n); err != nil {
+		log.Fatalln("Failed to parse address book:", err)
 	}
-
-	return content, nil
 }
